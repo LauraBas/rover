527f783bc85b1444cfe2f088dd36e2cc5454dce8
Laura Bassani <bassanilaurarita@gmail.com>
2020-10-04 16:56:11 +0200

    One rover finished

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,13 +1,37 @@
-import idx, {Got} from '../src/index';
+import ourProgram from '../src/index';
 
-const resp = idx();
+describe('our program', () => {
+  it('should return starting position if no movement is provided', () => {
+    const input = '3 3\n0 0 N\n';
 
-describe('test', () => {
-  it('should export Got function', () => {
-    expect(Got).toBeInstanceOf(Function);
+    const output = ourProgram(input);
+
+    expect(output).toEqual('0 0 N');
   });
 
-  it('should return hello world', () => {
-    expect(resp).toEqual('Hello World');
+  it('should return new position from input provided', () => {
+    expect(ourProgram('3 3\n0 0 N\nM')).toEqual('0 1 N');
+    expect(ourProgram('3 3\n0 0 N\nMM')).toEqual('0 2 N');
+    expect(ourProgram('5 5\n0 0 N\nMMMMM')).toEqual('0 5 N');
+  });
+
+  it('should return new orientation from input provided', () => {
+    expect(ourProgram('3 3\n0 0 N\nL')).toEqual('0 0 W');
+    expect(ourProgram('3 3\n0 0 N\nLL')).toEqual('0 0 S');
+    expect(ourProgram('3 3\n0 0 N\nLLL')).toEqual('0 0 E');
+    expect(ourProgram('3 3\n0 0 N\nLLLL')).toEqual('0 0 N');
+    expect(ourProgram('3 3\n0 0 N\nR')).toEqual('0 0 E');
+    expect(ourProgram('3 3\n0 0 N\nRR')).toEqual('0 0 S');
+  });
+  it('should return "Fall off" if exceed the plateau dimensions', () => {
+    expect(ourProgram('1 1\n0 0 N\nMM')).toEqual('Fall off');
+    expect(ourProgram('1 1\n0 0 S\nM')).toEqual('Fall off');
+    expect(ourProgram('1 3\n0 0 N\nMMRMMRMRRM')).toEqual('Fall off');
+  });
+
+  it('should work for happy path', () => {
+    expect(ourProgram('3 3\n0 0 N\nMMRMLML')).toEqual('1 3 W');
+    expect(ourProgram('5 5\n1 2 N\nLMLMLMLMM')).toEqual('1 3 N');
+    expect(ourProgram('5 5\n3 3 E\nMMRMMRMRRM')).toEqual('5 1 E');
   });
 });
--- src/index.js
+++ src/index.js
@@ -1,17 +1,83 @@
-export function Got() {
-  return 'Got';
-}
+export default function(input) {
+  let coordenates = input.split('\n');
+  let plateau = coordenates[0];
+  let plateauDimensions = plateau.split(' ');
+  let xPlateau = parseInt(plateauDimensions[0]);
+  let yPlateau = parseInt(plateauDimensions[1]);
+  let roverPosition = coordenates[1];
+  let roverInformation = roverPosition.split(' ');
+  let x = parseInt(roverInformation[0]);
+  let y = parseInt(roverInformation[1]);
+  let roverOrientation = roverInformation[2];
 
-export default function() {
-  return 'Hello World';
-}
-
-export async function fetchJson(url) {
-  try {
-    let request = await fetch(url);
-    let text = await request.text();
-    return JSON.parse(text);
-  } catch (error) {
-    console.log(`ERROR: ${error.stack}`);
+  function moveRover(c) {
+    if (c == 'M') {
+      move();
+    } else if (c == 'L') {
+      turnLeft();
+    } else {
+      turnRigth();
+    }
   }
+  function move() {
+    switch (roverOrientation) {
+      case 'N':
+        y += 1;
+        break;
+      case 'S':
+        y -= 1;
+        break;
+      case 'E':
+        x += 1;
+        break;
+      case 'W':
+        x -= 1;
+    }
+  }
+  function turnLeft() {
+    switch (roverOrientation) {
+      case 'N':
+        roverOrientation = 'W';
+        break;
+      case 'W':
+        roverOrientation = 'S';
+        break;
+      case 'S':
+        roverOrientation = 'E';
+        break;
+      case 'E':
+        roverOrientation = 'N';
+        break;
+    }
+  }
+
+  function turnRigth() {
+    switch (roverOrientation) {
+      case 'N':
+        roverOrientation = 'E';
+        break;
+      case 'E':
+        roverOrientation = 'S';
+        break;
+      case 'S':
+        roverOrientation = 'W';
+        break;
+      case 'W':
+        roverOrientation = 'N';
+        break;
+    }
+  }
+
+  let comands = coordenates[2].split('');
+
+  for (let i = 0; i < comands.length; i++) {
+    moveRover(comands[i]);
+    if (x > xPlateau || x < 0) {
+      return 'Fall off';
+    } else if (y > yPlateau || y < 0) {
+      return 'Fall off';
+    }
+  }
+  let roverNewPosition = x + ' ' + y + ' ' + roverOrientation;
+  return roverNewPosition;
 }
